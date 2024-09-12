1e0987a879184ece7ff821fffd5385194427d530
jack dryvers <j.dryvers@proton.me>
2024-09-12 13:48:51 +0900

    fixed some dependencies

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import torch
-from tts.api import TTS
+from TTS.api import TTS
+import os
+import numpy as np
+from playsound import playsound
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print (f"Using device: {device}")
+
+
+
+
+
 
 def generate_audio(text):
-    tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", device=device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print (f"Using device: {device}")
+    tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC").to(device)
     tts.tts_to_file(text=text, file_path="output.wav")
     return "output.wav"
 
+
+
+
+# Get current directory and set audio file path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+audio_file_path = os.path.join(current_dir, "..", "output.wav")
+
+
+
+
+
 if __name__ == "__main__":
-    text = "Hello, I am a text-to-speech model.Wow, this is amazing!"
-    generate_audio(text)
+    text = "M' bout' to go AWOL"
+    #generate_audio(text)
+    #play the audio file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    audio_file_path = os.path.join(current_dir, "..", "output.wav")
+    # Call the play_audio function
+    # Play the sound
+    playsound(audio_file_path)
