fbfa736f5c2c48baa8a59ece7c14c9f1fed3661f
jack dryvers <j.dryvers@proton.me>
2024-09-12 14:13:48 +0900

    WSL 2 does not have access to audio devices so no sound output fr now :(

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import torch
 from TTS.api import TTS
 import os
-from playsound import playsound
+import soundfile as sf
+import sounddevice as sd
+
+
 
 def generate_audio(text):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print (f"Using device: {device}")
-    tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC").to(device)
+    processing_device = "cuda" if torch.cuda.is_available() else "cpu"
+    print (f"Using device: {processing_device}")
+    tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC").to(processing_device)
     tts.tts_to_file(text=text, file_path="output.wav")
     return "output.wav"
 
 
 if __name__ == "__main__":
     text = "M' bout' to go AWOL"
-    #generate_audio(text)
-    #play the audio file
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    audio_file_path = os.path.join(current_dir, "output.wav")
-    # Call the play_audio function
-    # Play the sound
-    playsound(audio_file_path)
+    generate_audio(text)
+    # #play the audio file
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # sound_file = os.path.join(current_dir, "output.wav")
+
+
+    # print(sd.query_devices())
+    # # Read the sound file
+    # data, samplerate = sf.read(sound_file)
+
+    # # Play the sound
+    # sd.play(data, samplerate)
+
+    # # Wait until the sound finishes playing
+    # sd.wait()
