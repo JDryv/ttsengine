671fd0d979a996b8af9cdde186cdd297754d213d
jack dryvers <j.dryvers@proton.me>
2024-09-12 14:05:10 +0900

    trying soundfile library as playsound fails

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import torch
 from TTS.api import TTS
 import os
-import numpy as np
 from playsound import playsound
-
-
-
-
-
-
 
 def generate_audio(text):
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -18,22 +11,12 @@ def generate_audio(text):
     return "output.wav"
 
 
-
-
-# Get current directory and set audio file path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-audio_file_path = os.path.join(current_dir, "..", "output.wav")
-
-
-
-
-
 if __name__ == "__main__":
     text = "M' bout' to go AWOL"
     #generate_audio(text)
     #play the audio file
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    audio_file_path = os.path.join(current_dir, "..", "output.wav")
+    audio_file_path = os.path.join(current_dir, "output.wav")
     # Call the play_audio function
     # Play the sound
     playsound(audio_file_path)
