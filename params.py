49f2281df8d2522c3ad8258a378c4d6713930f92
jack dryvers <j.dryvers@proton.me>
2024-09-12 22:22:33 +0900

    cleaned packages

--- params.py
+++ params.py
@@ -1,6 +1,12 @@
-TTS_LOCAL = True
+
 TTS_ENGINE = "coqui-tts" # "coqui-tts" or "..."
 COQUI_ENGINE = "tacotron2-DDC" # "tacotron2-DDC" or "..."
+
+#------------------ DO NOT MODIFY BELOW THIS LINE ------------------#
+TTS_LOCAL_DICT = {
+    "coqui-tts": True
+}
+TTS_LOCAL = TTS_LOCAL_DICT[TTS_ENGINE]
 COQUI_ENGINE_DICT = {
     "tacotron2-DDC":"tts_models/en/ljspeech/tacotron2-DDC"
 }
